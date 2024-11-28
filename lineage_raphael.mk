19133509f131430327b22eb64a2b76d7281770f7
ralf979 <ralf979@gmail.com>
2024-11-28 22:18:06 +0100

    raphael: sign build

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -46,3 +46,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="raphael-user 11 RKQ1.200826.002 V12.5.2.0.RFKMIXM release-keys" \
     BuildFingerprint=Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
+
+# Inherit from release keys
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
